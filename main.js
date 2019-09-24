64ec586086f9243700a0a6968e43df8c72d8aa36
crazywickedawesome <ian@crazywickedawesome.com>
2019-09-24 18:08:24 -0400

    scraps a start button. Should be a cleaner UI now. Draws a circle. note that circle styles are not applying. Fix coming soon.

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-let stepOne = document.createElement ("h1");
+var circle = document.getElementById("breath");
+var ctx = circle.getContext('2d');
+ctx.beginPath();
+ctx.arc(100, 75, 75, 1, 2 * Math.PI);
+ctx.strokeStyle = "#FFFFF";
+ctx.stroke();
+console.log(ctx); // logs details about render to console;
+
+let stepOne = document.createElement ("p");
 let sOText = document.createTextNode("This is new.");
 stepOne.appendChild(sOText);
 sOText.className = "guide";
